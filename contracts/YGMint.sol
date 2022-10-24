94323e022d54e81c993b20158ca1718aa71347d3
ethan <sdzdj01@163.com>
2022-10-24 13:39:24 +0800

    new file:   contracts/YGMint.sol 	new file:   contracts/YgmMarket.sol

---
+++ contracts/YGMint.sol
@@ -0,0 +1,255 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract ERC721URIStorage is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+    bool public isStart;
+
+    string openedUrl;
+
+    uint256 lastOpenTokenId;
+
+    // Optional mapping for token URIs
+    mapping(uint256 => string) private _tokenURIs;
+
+    function start() external onlyOwner {
+        isStart = !isStart;
+    }
+
+    function open(string memory url, uint256 lastTokenId) external onlyOwner {
+        if (0 == bytes(openedUrl).length) {
+            openedUrl = url;
+        }
+
+        require(
+            0 < lastTokenId &&
+                lastOpenTokenId < lastTokenId &&
+                lastTokenId <= totalSupply(),
+            "Invalid tokenid"
+        );
+
+        lastOpenTokenId = lastTokenId;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI query for nonexistent token"
+        );
+
+        if (bytes(openedUrl).length > 0 && tokenId <= lastOpenTokenId) {
+            return string(abi.encodePacked(openedUrl, "/", tokenId.toString()));
+        }
+
+        return _baseURI();
+    }
+
+    /**
+     * @dev Sets `_tokenURI` as the tokenURI of `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+        internal
+        virtual
+    {
+        require(
+            _exists(tokenId),
+            "ERC721URIStorage: URI set of nonexistent token"
+        );
+        _tokenURIs[tokenId] = _tokenURI;
+    }
+
+    /**
+     * @dev Destroys `tokenId`.
+     * The approval is cleared when the token is burned.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _burn(uint256 tokenId) internal virtual override {
+        super._burn(tokenId);
+
+        if (bytes(_tokenURIs[tokenId]).length != 0) {
+            delete _tokenURIs[tokenId];
+        }
+    }
+}
+
+contract YGMint is ERC721URIStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter = Counters.Counter(1);
+
+    // uint mintTotal = 6666;
+
+    // uint teamMax = 666;
+    // uint teamMint = 0;
+
+    // uint mintMax = 12;
+    // uint mintFee = 10;
+
+    // uint mintEveryAmount = 12 * 10 ** 17;
+    // uint firstLeave = 1 * 10 ** 17;
+    // uint secondtLeave = 5 * 10 ** 16;
+
+    uint256 mintTotal = 66;
+
+    uint256 teamMax = 6;
+    uint256 teamMint = 0;
+
+    uint256 mintMax = 12;
+    uint256 mintFee = 10;
+
+    uint256 mintEveryAmount = 12 * 10**15;
+    uint256 firstLeave = 1 * 10**15;
+    uint256 secondtLeave = 5 * 10**14;
+
+    mapping(address => bool) public isMint;
+
+    mapping(address => address payable) recommender;
+
+    constructor() ERC721("YGM", "YGM") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmPAcZGzXzcrCfs399nASK2SJGEByjZr3atBpGML4vzfj1";
+    }
+
+    function getAmount(uint256 mintCount) external view returns (uint256) {
+        require(
+            totalSupply() + mintCount <= mintTotal,
+            "Maximum mint quantity exceeded"
+        );
+        address minter = _msgSender();
+        uint256 balanceMiner = balanceOf(minter);
+        uint256 balanceWill = balanceMiner + mintCount;
+        require(0 < mintCount && balanceWill <= mintMax, "Invalid quantity");
+        if (balanceWill > mintFee) {
+            if (balanceMiner < mintFee) {
+                uint256 feeCount = mintFee - balanceMiner;
+                return feeCount * mintEveryAmount;
+            } else {
+                return 0;
+            }
+        } else {
+            return mintCount * mintEveryAmount;
+        }
+    }
+
+    function safeMint(address payable _recommender, uint256 mintCount)
+        external
+        payable
+        checkRecommender(_recommender)
+    {
+        require(isStart, "Not started");
+
+        require(
+            totalSupply() + mintCount <= mintTotal,
+            "Maximum mint quantity exceeded"
+        );
+
+        address minter = _msgSender();
+        uint256 balanceMiner = balanceOf(minter);
+        uint256 balanceWill = balanceMiner + mintCount;
+        require(0 < mintCount && balanceWill <= mintMax, "Invalid quantity");
+
+        if (balanceWill > mintFee) {
+            if (balanceMiner < mintFee) {
+                uint256 feeCount = mintFee - balanceMiner;
+                uint256 mintAmount = feeCount * mintEveryAmount;
+                require(
+                    mintAmount <= msg.value,
+                    "Insufficient payment quantity"
+                );
+                if (mintAmount < msg.value) {
+                    payable(minter).transfer(msg.value - mintAmount);
+                }
+                rewardRecommender(minter, feeCount);
+            } else if (msg.value > 0) {
+                payable(minter).transfer(msg.value);
+            }
+        } else {
+            uint256 mintAmount = mintCount * mintEveryAmount;
+            require(mintAmount <= msg.value, "Insufficient payment quantity");
+            if (mintAmount < msg.value) {
+                payable(minter).transfer(msg.value - mintAmount);
+            }
+            rewardRecommender(minter, mintCount);
+        }
+
+        for (uint256 i = 0; i < mintCount; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(minter, tokenId);
+        }
+
+        if (!isMint[minter]) {
+            isMint[minter] = true;
+        }
+    }
+
+    function mint(address to, uint256 count) external onlyOwner {
+        require(teamMint < teamMax, "Mint end");
+        require(count > 0, "count can not be zero");
+        for (uint256 i = 0; i < count; i++) {
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(to, tokenId);
+        }
+        teamMint += count;
+
+        if (!isMint[to]) {
+            isMint[to] = true;
+        }
+    }
+
+    function withdrawal(address payable to, uint256 amount) external onlyOwner {
+        require(address(0) != to, "To can not be zero");
+        to.transfer(amount);
+    }
+
+    function rewardRecommender(address minter, uint256 count) private {
+        address zero = address(0);
+
+        address payable firstRecommender = recommender[minter];
+        if (zero != firstRecommender && isMint[firstRecommender]) {
+            firstRecommender.transfer(firstLeave * count);
+        }
+
+        address payable secondRecommender = recommender[firstRecommender];
+        if (zero != secondRecommender && isMint[secondRecommender]) {
+            secondRecommender.transfer(secondtLeave * count);
+    }
+
+    modifier checkRecommender(address payable _recommender) {
+        if (recommender[msg.sender] == address(0)) {
+            require(address(0) != _recommender, "Recommender can not be zero");
+            require(
+                msg.sender != _recommender,
+                "Recommender can not to be same"
+            );
+            require(isMint[_recommender], "Recommender no mint");
+            recommender[msg.sender] = _recommender;
+        }
+        _;
+    }
+}
---
+++ contracts/YgmMarket.sol
@@ -0,0 +1,261 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract YgmMarket is Ownable {
+    mapping(address => bool) public noSell;
+
+    uint256 maxFee = 10000;
+    uint256 defFee;
+    address platformAddress;
+    mapping(address => uint256) platformFee;
+
+    mapping(address => uint256) public businessFee;
+    mapping(address => address) public businessAddress;
+    mapping(address => address) public payContract;
+
+    mapping(address => mapping(uint256 => uint256)) tokenAmount;
+
+    mapping(address => mapping(uint256 => bool)) isLock;
+
+    event BuildProject(
+        address indexed _contract,
+        address _payContract,
+        uint256 _businessFee,
+        address _businessAddress
+    );
+    event Shelves(
+        address indexed seller,
+        address indexed _contract,
+        uint256 indexed tokenId,
+        address _payContract,
+        uint256 amount,
+        uint256 timestamp
+    );
+    event Cancel(
+        address indexed seller,
+        address indexed _contract,
+        uint256 indexed tokenId,
+        address _payContract,
+        uint256 amount,
+        uint256 timestamp
+    );
+    event Buy(
+        address indexed seller,
+        address indexed buyer,
+        address _contract,
+        uint256 indexed tokenId,
+        address _payContract,
+        uint256 amount,
+        uint256 platformRate,
+        uint256 businessRate,
+        uint256 timestamp
+    );
+
+    constructor(address _address, uint256 _rate) {
+        setPlatformAddress(_address);
+        setPlatformDefFee(_rate);
+    }
+
+    function getPlatformAddress() external view onlyOwner returns (address) {
+        return platformAddress;
+    }
+
+    function getPlatformFee(address _contract) public view returns (uint256) {
+        if (address(0) == _contract) {
+            return defFee;
+        } else {
+            return
+                0 == platformFee[_contract] ? defFee : platformFee[_contract];
+        }
+    }
+
+    function getSellAmount(address _contract, uint256 _tokenId)
+        external
+        view
+        returns (uint256)
+    {
+        return tokenAmount[_contract][_tokenId];
+    }
+
+    function _getFee(
+        uint256 _amount,
+        uint256 _platformRate,
+        uint256 _businessRate
+    ) private view returns (uint256) {
+        uint256 sumRate = _platformRate + _businessRate;
+        return (_amount * sumRate) / maxFee;
+    }
+
+    function buildProject(
+        address _contract,
+        address _payContract,
+        uint256 _businessFee,
+        address _businessAddress
+    ) external _nft_owner(_contract) {
+        payContract[_contract] = _payContract;
+
+        uint256 feeTotal = _businessFee + getPlatformFee(_contract);
+        require(feeTotal < maxFee, "Fee too large");
+        businessFee[_contract] = _businessFee;
+
+        if (address(0) == _businessAddress) {
+            address sender = _msgSender();
+            businessAddress[_contract] = sender;
+        } else {
+            businessAddress[_contract] = _businessAddress;
+        }
+
+        emit BuildProject(
+            _contract,
+            _payContract,
+            _businessFee,
+            _businessAddress
+        );
+    }
+
+    function shelves(
+        address _contract,
+        uint256 tokenId,
+        uint256 amount
+    ) external {
+        require(!noSell[_contract], "The nft can not sell");
+
+        require(0 == tokenAmount[_contract][tokenId], "Nft is selling");
+
+        address sender = _msgSender();
+        IERC721 nft = IERC721(_contract);
+        require(
+            sender == IERC721(_contract).ownerOf(tokenId),
+            "Nft owner is not sender"
+        );
+
+        require(address(this) == nft.getApproved(tokenId), "Nft unapprove");
+
+        tokenAmount[_contract][tokenId] = amount;
+
+        emit Shelves(
+            sender,
+            _contract,
+            tokenId,
+            address(0) == payContract[_contract]
+                ? address(0)
+                : payContract[_contract],
+            amount,
+            block.timestamp
+        );
+    }
+
+    function cancel(address _contract, uint256 tokenId) external {
+        require(0 < tokenAmount[_contract][tokenId], "Nft is canceled");
+
+        address sender = _msgSender();
+
+        IERC721 nft = IERC721(_contract);
+
+        require(sender == nft.ownerOf(tokenId), "Nft owner is not sender");
+
+        uint256 sellAmount = tokenAmount[_contract][tokenId];
+        tokenAmount[_contract][tokenId] = 0;
+
+        emit Cancel(
+            sender,
+            _contract,
+            tokenId,
+            address(0) == payContract[_contract]
+                ? address(0)
+                : payContract[_contract],
+            sellAmount,
+            block.timestamp
+        );
+    }
+
+    function buy(address _contract, uint256 tokenId)
+        external
+        payable
+        locked(_contract, tokenId)
+    {
+        require(!noSell[_contract], "The nft can not sell");
+
+        uint256 sellAmount = tokenAmount[_contract][tokenId];
+        require(0 < sellAmount, "Nft already buyed");
+
+        IERC721 nft = IERC721(_contract);
+        address _nftOwner = nft.ownerOf(tokenId);
+        address _sender = _msgSender();
+
+        uint256 _platformRate = getPlatformFee(_contract);
+        uint256 _businessRate = businessFee[_contract];
+        uint256 fee = _getFee(sellAmount, _platformRate, _businessRate);
+
+        address payType = payContract[_contract];
+        if (address(0) == payType) {
+            require(msg.value == sellAmount, "Pay amount invalid");
+            payable(platformAddress).transfer(fee);
+            payable(_nftOwner).transfer(sellAmount - fee);
+        } else {
+            IERC20 erc = IERC20(payType);
+            uint256 _allowance = erc.allowance(_sender, address(this));
+            require(
+                _allowance >= sellAmount,
+                "Insufficient number of authorizations"
+            );
+            erc.transferFrom(_sender, platformAddress, fee);
+            erc.transferFrom(_sender, _nftOwner, sellAmount - fee);
+        }
+
+        nft.safeTransferFrom(_nftOwner, _sender, tokenId);
+        tokenAmount[_contract][tokenId] = 0;
+
+        emit Buy(
+            _nftOwner,
+            _sender,
+            _contract,
+            tokenId,
+            payType,
+            sellAmount,
+            _platformRate,
+            _businessRate,
+            block.timestamp
+        );
+    }
+
+    function swichSell(address _contract) external onlyOwner {
+        noSell[_contract] = !noSell[_contract];
+    }
+
+    function setPlatformDefFee(uint256 _amount) public onlyOwner {
+        defFee = _amount;
+    }
+
+    function setPlatformAddress(address _address) public onlyOwner {
+        platformAddress = _address;
+    }
+
+    function setPlatformFee(address _contract, uint256 _amount)
+        external
+        onlyOwner
+    {
+        platformFee[_contract] = _amount;
+    }
+
+    // check nft owner
+    modifier _nft_owner(address _contract) {
+        address sender = _msgSender();
+
+        address _owner = Ownable(_contract).owner();
+
+        require(_owner == sender, "Not owner");
+
+        _;
+    }
+
+    modifier locked(address _contract, uint256 tokenId) {
+        require(!isLock[_contract][tokenId], "Nft is buying");
+        isLock[_contract][tokenId] = true;
+        _;
+        isLock[_contract][tokenId] = false;
+    }
+}
