9f3206077b36460854d74b90f840b89b1cbf1ddf
ethan <sdzdj01@163.com>
2022-10-25 15:07:56 +0800

    modified:   scripts/getSignature.js

--- scripts/getSignature.js
+++ scripts/getSignature.js
@@ -1,34 +1,27 @@
 const { ethers } = require("hardhat");
 
-// const provider = new ethers.providers.JsonRpcProvider(
-//   process.env.ALCHEMY_GOERLI_URL
-// );
-
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 
-// // 签名文本消息
-// let str = "Hello";
-// let signPromise = wallet.signMessage(str);
-
-// console.log(signPromise);
-
-// signPromise.then((signature) => {
-//   // Flat-format
-//   console.log(signature);
-
-//   // Expanded-format
-//   // console.log(ethers.utils.splitSignature(signature));
-// });
-
-// 签名 msg.data
+// 签名二进制信息
+// https://learnblockchain.cn/docs/ethers.js/api-wallet.html#id14
+// https://learnblockchain.cn/docs/ethers.js/api-utils.html#solidity
+// keccak256(abi.encodePacked(a, b)) == utils.solidityKeccak256(types, values)
+let tokenId = 10;
+let account = "0x6278a1e803a76796a3a1f7f6344fe874ebfe94b2";
 let message = ethers.utils.solidityKeccak256(
   ["uint256", "address"],
-  [5, "0x6278A1E803A76796a3A1f7F6344fE874ebfe94B2"]
+  [tokenId, account]
 );
-console.log(message);
-console.log(ethers.utils.arrayify(message));
+console.log(`tokenId: ${tokenId}`);
+console.log(`account: ${account}`);
+console.log("utils.solidityKeccak256(a, b):", message);
+// console.log(ethers.utils.arrayify(message));
 const signature = wallet.signMessage(ethers.utils.arrayify(message));
-console.log(signature);
+
+signature.then((sign) => {
+  console.log("signature:", sign);
+});
+// console.log(signature);
 
 // https://goerli.etherscan.io/address/0x6e7f9fccadfd34689a9542534c25475b5ffb7282#code
 
