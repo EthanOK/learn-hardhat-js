ba2153012aa234b2001d6df9821fecc102fcbea5
ethan <sdzdj01@163.com>
2022-11-01 12:32:32 +0800

    new file:   contracts/ERC721ATest.sol 	modified:   package-lock.json 	modified:   package.json

---
+++ contracts/ERC721ATest.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "erc721a/contracts/ERC721A.sol";
+
+contract ERC721ATest is ERC721A {
+    string baseURI =
+        "ipfs://bafybeibnjk2a57x2sgil3yfxck4mqxjgo4ngyyp52kaiax63l5ty4fgf2q";
+
+    constructor() ERC721A("ERC721ATest", "ERC721AT") {}
+
+    function mint(uint256 quantity) external payable {
+        // `_mint`'s second argument now takes in a `quantity`, not a `tokenId`.
+        _mint(msg.sender, quantity);
+    }
+
+    // override _startTokenId(default: tokenId start 0)
+    function _startTokenId() internal pure override returns (uint256) {
+        return 1;
+    }
+
+    // override _baseURI
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    // override tokenURI
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+        return
+            bytes(baseURI).length != 0
+                ? string(abi.encodePacked(baseURI, "/", _toString(tokenId)))
+                : "";
+    }
+}
