32ba8cc69e51fb03cbd0cb35a4d115151927f0f7
ethan <sdzdj01@163.com>
2022-11-17 11:59:13 +0800

    new file:   contracts/ERC721ConsecutiveTest.sol 	modified:   package.json

---
+++ contracts/ERC721ConsecutiveTest.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Consecutive.sol";
+contract ERC721ConsecutiveTest is ERC721Consecutive{
+    constructor()ERC721("YGG","YG"){
+        
+    }
+}
