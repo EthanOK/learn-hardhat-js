76944be188800348a2b6356694e9824b81b05ed4
ethan <sdzdj01@163.com>
2022-11-23 10:28:29 +0800

    modified:   contracts/yg-contracts/YgmStaking.sol

--- contracts/yg-contracts/YgmStaking.sol
+++ contracts/yg-contracts/YgmStaking.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 abstract contract YgmStakingBase is Ownable, Pausable {
