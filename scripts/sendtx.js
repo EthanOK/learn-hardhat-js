1b2c1a045eb00e45bdd39fbe91394c2f7518fb3a
ethan <sdzdj01@163.com>
2022-11-30 18:25:11 +0800

    modified:   scripts/sendtx.js

--- scripts/sendtx.js
+++ scripts/sendtx.js
@@ -1,30 +1,44 @@
 const { ethers } = require("hardhat");
-async function sendTransaction() {
-  const [owner] = await ethers.getSigners();
-  console.log(owner);
-  let wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
 
-  console.log(wallet.address);
+async function sendtx() {
+  provider = new ethers.providers.JsonRpcProvider();
+  pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
-  // All properties are optional
-  let transaction = {
-    to: "0x53188E798f2657576c9de8905478F46ac2f24b67",
+  wallet = new ethers.Wallet(pk);
+
+  // Wallet connected to a provider
+  wallet = await wallet.connect(provider);
+
+  // Signing a message
+  await wallet.signMessage("Hello World");
+
+  txdata = {
+    to: "0x8ba1f109551bD432803012645Ac136ddd64DBA72",
     value: ethers.utils.parseEther("1.0"),
-    data: "0x",
-    // 这可确保无法在不同网络上重复广播
-    chainId: 12,
   };
 
-  let signPromise = wallet.sign(transaction);
+  // Signing a transaction
+  hashdata = await wallet.signTransaction(txdata);
+  console.log("signTransaction hashdata:" + hashdata);
 
-  signPromise.then((signedTransaction) => {
-    console.log(signedTransaction);
+  count = await wallet.getTransactionCount();
+  console.log("TransactionCount:" + count);
 
-    // 现在可以将其发送到以太坊网络
-    let provider = ethers.getDefaultProvider();
-    provider.sendTransaction(signedTransaction).then((tx) => {
-      console.log(tx);
-    });
-  });
+  // Sending ether
+  TResponse = await wallet.sendTransaction(txdata);
+  await TResponse.wait();
+
+  addr = await wallet.getAddress();
+  console.log("addr: " + addr);
+  balance = await provider.getBalance(addr);
+  console.log("send balance: " + balance);
+
+  balance = await provider.getBalance(
+    "0x8ba1f109551bD432803012645Ac136ddd64DBA72"
+  );
+  console.log("receive balance: " + balance);
+
+  count = await wallet.getTransactionCount();
+  console.log("TransactionCount:" + count);
 }
-sendTransaction();
+sendtx();
